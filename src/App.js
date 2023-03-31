d3a76318aed9836239292db8d009849cd33fcb7b
Morteza Fard Saffari <mortezafardsaffari@yahoo.com>
2023-03-31 19:26:32 +0330

    Add Commpnents in main page

--- src/App.js
+++ src/App.js
@@ -3,34 +3,42 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 import { AppProvider } from './context/AppContext';
-import CartValue from './components/CartValue';
+import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import Spent from './components/Spent';
 import ExpenseList from './components/ExpenseList';
-import ItemSelected from './components/ItemSelected';
-import Location from './components/Location';
+import AllocationForm from './components/AllocationForm';
+import Currencies from './components/Currencies';
 
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
-                <h1 className='mt-3'>Shopping App</h1>
+                <h1 className='mt-3'>Company's Budget Allocation</h1>
                 <div className='row mt-3'> 
                     <div className='col-sm'>
-                        <CartValue />
+                        <Budget />
                     </div>
                     <div className='col-sm'>
-                        <Location />
+                        <Remaining />
+                    </div>
+                    <div className='col-sm'>
+                        <Spent />
+                    </div>
+                    <div className='col-sm'>
+                        <Currencies />
                     </div>
                 </div>
-                <h3 className='mt-3'>Shopping Cart</h3>
+                <h3 className='mt-3'>Allocation</h3>
                 <div className='row '>
                     <div className='col-sm'>
                         <ExpenseList />
                     </div>
                 </div>
-                <h3 className='mt-3'>Add Items</h3>
+                <h3 className='mt-3'>Change Allocation</h3>
                 <div className='row mt-3'>
                     <div className='col-sm'>
-                        <ItemSelected/>
+                        <AllocationForm/>
                     </div>
                 </div>
             </div>
